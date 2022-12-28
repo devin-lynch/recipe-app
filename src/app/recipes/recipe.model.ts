f3730e14f7b5e71cd348d8653fde3f08c8720f57
devin-lynch <devin.l.lynch@gmail.com>
2022-12-27 20:12:33 -0600

    Create recipe model

---
+++ src/app/recipes/recipe.model.ts
@@ -0,0 +1,11 @@
+export class Recipe { // creating a model for the recipe so we have a consistent structure!
+  public name: string;
+  public description: string;
+  public imagePath: string;
+
+  constructor(name: string, description: string, imagePath: string) {
+    this.name = name;
+    this.description = description;
+    this.imagePath = imagePath;
+  }
+}
